2b357a8d13b33ee7c135c18141521d6f4f5c3b3c
mayankmishra0801 <mayank08mrinal04@gmail.com>
2023-03-17 11:05:40 +0530

    cookie

--- shop_app/src/app/app.component.ts
+++ shop_app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shop_app';
+
+  constructor(private cookie:CookieService){
+
+  }
+
+  SetCookie(){
+    this.cookie.set("userId","adminuser");
+    this.cookie.set("username","ramesh");
+  }
+
+  GetCookie(){
+     console.log(this.cookie.get("username"));
+  }
+
+  removeCookie(){
+     this.cookie.delete("username")
+  }
+  removeallCookie(){
+        this.cookie.deleteAll();
+  }
+
 }
--- shop_app/src/app/pages/profile/profile.component.ts
+++ shop_app/src/app/pages/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
-
+// import {Cook}
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
